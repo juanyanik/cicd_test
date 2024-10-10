2f992a6678a13c5c0bcf854fe39bd4f5265b4a36
juanyanik <juan.yanik6@gmail.com>
2024-10-10 16:22:09 -0300

    new schema

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "0.25.17"
     }
   }
-  
+
   backend "remote" {
     organization = "cicd-test"
 
@@ -21,4 +21,10 @@ provider "snowflake" {
 resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
+}
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
 }
