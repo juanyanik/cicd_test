2c8f488643d5d478d7aa40f56f5ba760b1a83491
juanyanik <juan.yanik6@gmail.com>
2024-10-18 18:17:47 -0300

    test

---
+++ migrations/V1.1.3__new_db.sql
@@ -0,0 +1,21 @@
+-- Create demo_db_dev with schema and table
+CREATE DATABASE demo_db_dev;
+
+USE DATABASE demo_db_dev;
+
+CREATE SCHEMA demo_schema_dev;
+
+CREATE TABLE demo_schema_dev.demo_table (
+    id INT,
+    name STRING
+);
+
+-- Insert values into the table
+INSERT INTO demo_schema_dev.demo_table (id, name)
+VALUES 
+    (1, 'Alice'),
+    (2, 'Bob'),
+    (3, 'Charlie');
+
+-- Create empty demo_db_prod
+CREATE DATABASE demo_db_prod;
