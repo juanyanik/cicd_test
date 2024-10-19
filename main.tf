d3be5fdd749c257f0bbb27d9211dbd95ea6173e5
juanyanik <100418934+juanyanik@users.noreply.github.com>
2024-10-19 08:17:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,9 +28,3 @@ resource "snowflake_schema" "demo_schema" {
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
-
-resource "snowflake_schema" "demo_schema2" {
-  database = snowflake_database.demo_db.name
-  name     = "DEMO_SCHEMA2"
-  comment  = "Schema for Snowflake Terraform demo"
-}
